7658d02c3a6dd2cbcf9302f08ea7b531e416bcd1
sudovazid <vazidmail@protonmail.com>
2022-04-02 17:10:58 +0530

    projects

---
+++ Kylie Ying/Guess number/main.py
@@ -0,0 +1,5 @@
+import random
+
+def guess(x):
+    random_number = random.randiant(1, x)
+    
---
+++ acronyms.py
@@ -0,0 +1,6 @@
+user_input = str(input("Enter a phrase: "))
+text = user_input.split()
+a = " "
+for i in text:
+    a = a+str(i[0]).upper()
+print(a)
